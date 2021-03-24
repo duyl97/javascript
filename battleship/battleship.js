e5f833e8d35392c2249c18d50c4a16ff3daa8b3e
duyl97 <dl97.app@gmail.com>
2021-03-24 21:09:53 +0700

    update battleship game

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -7,10 +7,25 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false) {
-    guess = prompt("Ready, aim, fire! (enter a number 0-6:");
+    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
     if (guess < 0 || guess > 6) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits +1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleships!");
+            }
+        } else {
+            alert("MISS!");
+        }
     }
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accurancy was " + (3/guesses);
+
+alert(stats);
